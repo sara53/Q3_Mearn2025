8e389236647a58f1bbffcd2844c2aba338b665fa
sara53 <sara.atef001@gmail.com>
2025-03-12 11:32:31 +0200

    day04

---
+++ JS/day04/main.js
@@ -0,0 +1,70 @@
+// var div = document.getElementById("content");
+// var result = document.getElementById("result");
+// console.log(div.firstChild);
+// console.log(result.firstChild);
+// console.log(document.forms); // form
+// console.log(document.links);
+// console.log(document.body); // select body
+// var div = document.getElementById("content");
+// var element = document.querySelector("#header");
+// console.log(element);
+// for (var i = 0; i < elements.length; i++) {
+// 	console.log(elements[i]);
+// }
+// function showData() {
+// 	console.log("Btn Clicked");
+// }
+
+// var changeColor = function () {
+// 	console.log("Change Coloe");
+// };
+// var p = document.getElementsByClassName("head")[0];
+// function changeContent() {
+// 	console.log(p.innerHTML);
+// 	console.log(p.innerText);
+// 	p.innerText = "<h1>Hello mearn track</h1>";
+// 	console.log(p.innerText); // get
+// 	p.innerHTML = "<h1>Hello Mearn track</h1>";
+// 	p.innerHTML = "Hello Mearn track";
+// 	h1.innerText = "Hello Mearn track";
+// 	get;
+// 	console.log(h1.innerText);
+// }
+
+// var person = {
+// 	fname: "Ali",
+// };
+
+// console.log(person.fname); // fname
+
+// person.fname = "ahmed"; // set
+/** Change Content */
+
+// var input1 = document.getElementById("input1");
+// var result = document.getElementById("result");
+
+// var inputs = document.getElementsByName("choose");
+// function execute() {
+// 	for (var i = 0; i < inputs.length; i++) {
+// 		if (inputs[i].checked) {
+// 			console.log(inputs[i].value);
+// 		}
+// 	}
+// 	result.innerHTML = input1.value;
+// 	input1.value = "MEarn";
+// 	console.log(input1.value); // get
+// }
+// function changeStyle() {
+// 	// p.style.cssText = "background-color :red;color :green";
+// 	// p.style.backgroundColor = "#72004b";
+// 	// p.style.color = "#ddd";
+// }
+// function changeStyle() {
+// 	//set Attribute
+// 	p.classList.toggle("jsClass");
+// 	p.classList.add("jsClass");
+// 	p.classList.remove("testData");
+// 	console.log(p.classList);
+// 	p.className += " jsClass";
+// 	console.log(p.className); // get
+// }
